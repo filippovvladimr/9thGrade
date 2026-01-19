6e51ea93c1457579149b4b0ca9ca16053bc4d026
vlf_141 <160611970+filippovvladimr@users.noreply.github.com>
2026-01-19 17:35:57 +0300

    Create InsertionsSort.py

---
+++ 3Quater/2026-01-16/InsertionsSort.py
@@ -0,0 +1,13 @@
+def InsertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+    return arr
+
+
+print(InsertionSort([1,3,4,2,1,5,6,7,8,4]))
