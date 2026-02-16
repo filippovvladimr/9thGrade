3f557b4ba79596b12c05ed11c2136203db20542b
vlf_141 <filipovladimir@yandex.ru>
2026-02-16 14:37:49 +0300

    Update

--- 3Quater/2026-02-16/16febClassWork.py
+++ 3Quater/2026-02-16/16febClassWork.py
@@ -2,4 +2,10 @@ import numpy as np
 array1 = np.array([1,2,3])
 array2 = np.array([4,5,6])
 
-print(array1+array2)
+print(np.dot(array1,array2))
+print(array1.shape)
+
+
+array3 = np.zeros(5, dtype=int)
+array4 = np.ones([3,3], dtype=int)
+print(array4)
